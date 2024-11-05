068f8b664ddd4539fa713d63ca0592b0682f6bf3
sardiniaa <146239970+sardiniaa@users.noreply.github.com>
2024-11-05 17:54:42 +0100

    Add colors.js (CONSOLE && EMBED)

---
+++ colors.js
@@ -0,0 +1,35 @@
+//CONSOLE COLORS
+const REST = "\x1b[0m";
+const black = "\x1b[30m";
+const RED = "\x1b[31m";
+const GREEN = "\x1b[32m";
+const YELLOW = "\x1b[33m";
+const BLUE = "\x1b[34m";
+const MAGENTA = "\x1b[35m";
+const CYAN = "\x1b[36m";
+const white = "\x1b[37m";
+
+//EMBED COLORS
+const DEFAULT = "#ff0000";
+const ERROR = "#fe0000";
+const SUCCESS = "#00fe08";
+const PENDING = "#fea500";
+const LOGS = "#2b2d31"
+
+//EXPORT ALL COLORS
+module.exports = {
+    REST,
+    black,
+    RED,
+    GREEN,
+    YELLOW,
+    BLUE,
+    MAGENTA,
+    CYAN,
+    white,
+    DEFAULT,
+    ERROR,
+    SUCCESS,
+    PENDING,
+    LOGS
+};
